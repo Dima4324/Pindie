9cd6d4f7bc783ecbc4591667f61a953400d0c9a1
Dmitry (ubuntu) <ocean.of.blood@mail.ru>
2024-05-28 14:39:09 +0000

    fix errors

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://pindie-back-practicum.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://pindie-backend-practicum.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`
+  me: `${BASE_URL}/me`
 }; 
